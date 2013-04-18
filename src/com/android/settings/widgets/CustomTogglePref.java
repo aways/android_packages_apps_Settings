c4b138f69d99acc0926056071646a442dbe8bd9d
aways <aways@outlook.com>
2013-04-18 02:34:47 +0200

    C-RoM Part-4

--- src/com/android/settings/widgets/CustomTogglePref.java
+++ src/com/android/settings/widgets/CustomTogglePref.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
-import com.android.settings.carbon.StatusBarToggles;
+import com.android.settings.crom.StatusBarToggles;
 import com.android.settings.R;
 
 public class CustomTogglePref extends Preference {
